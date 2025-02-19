588f69be65eecee5017347f0cff374d264455026
Vishwas_kadam642 <vishwaskadam642@gmail.com>
2025-02-19 16:19:56 +0530

    written different datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,23 @@
+//Data Types
+
+//number datatype :- it has value range upto 2 to power 53
+let studentId = 34566
+
+//bigint :- it is used when range of number is larger than number datatupe range
+
+// string :- we use this datatype to declare string value it is written in ("").
+let studentName = "vishwas"
+
+//boolean :-we use this datatype when me want to declare true/false value to variable
+let accountLogin = true
+
+//null :- this datatype has standalone value
+let state = null ;
+
+// undefined :- this datatype we see when we not declare value to variable 
+let status;
+
+//symbol :- this datatype can contain both 1.built -in object, 2.user defined object 
+//examples for user defined objects 1.classes, 2.constractor function, 3.literals
+//examples for built -in objects 1.objects, 2.arrays, 3.dates, 4.maps, 5.sets, 6.intarrays, 7.floatarrays, 8.promises and more.
+  
