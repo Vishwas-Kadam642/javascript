9ec4c8d05cb06bbd1c91e3291a81c4d85b92ade9
Vishwas_kadam642 <vishwaskadam642@gmail.com>
2025-02-19 17:36:13 +0530

    code added to 03_conversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score1 = 33
+let score2 = "33abc"
+let score3 = true
+let score4 = null
+let score5 = undefined
+
+
+console.log(typeof score1);
+console.log(typeof score2);
+console.log(typeof score3);
+console.log(typeof score4);
+console.log(typeof score5);
+
+
+let valueNumber1 = String(score1)
+//conversion to number 
+let valueNumber2 = Number(score2)
+let valueNumber3 = Number(score3)
+let valueNumber4 = Number(score4)
+let valueNumber5 = Number(score5)
+console.log(typeof valueNumber1);
+console.log(typeof valueNumber2);
+console.log(typeof valueNumber3);
+console.log(typeof valueNumber4);
+console.log(typeof valueNumber5);
+console.log(valueNumber1);
+console.log(valueNumber2);
+console.log(valueNumber3);
+console.log(valueNumber4);
+console.log(valueNumber5);
