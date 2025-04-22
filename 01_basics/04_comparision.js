aa0761b8afaa2320ccea192e917bfb9f070f2927
Vishwas_kadam642 <vishwaskadam642@gmail.com>
2025-04-22 19:13:13 +0530

    nothing added

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,25 @@
+// // basics conversion operation
+// console.log(2 > 1); 
+// console.log(2 >= 1); 
+// console.log(2 < 1); 
+// console.log(2 == 1); 
+// console.log(2 != 1); 
+
+//  compare two strings
+
+//  console.log("2" > 1);
+//     console.log("02" > 1);
+
+
+
+
+// performing operations on null and undefined
+
+//  console.log(null > 0);
+//  console.log(null == 0);
+//  console.log(null >= 0);
+
+//  note: the equality check == and comparisons > < >= <= work differently.
+//  comarisons convert null to a number, treating it as 0. that's why (null > 0) and (null == 0) are false.
+
+// console.log(undefined == 0);
